2343ec740eab3265510137d3b18d0c6c55ce2cb9
Noemi Pusceddu <m_emi94@hotmail.it>
2023-09-08 16:02:47 +0200

    finish

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -10,137 +10,151 @@ REGOLE
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ['dog', 'cat', 'hamster', 'redfish']
-
-console.log(pets)
+// const pets = ['dog', 'cat', 'hamster', 'redfish']
+// for (let i = 0; i < pets.length; i++) {
+//   console.log(pets[i])
+// }
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
 
-pets.sort()
-console.log(pets)
+// let one = pets.sort()
+// console.log(one)
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
-pets.reverse()
-console.log(pets)
+// let two = pets.reverse()
+// console.log(two)
 
-/* ESERCIZIO 4
+/* ESERCIZIO 4 
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+
+// pets.splice(0, 1)
+// pets.push('redfish')
+
+// console.log(pets)
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
 */
-const cars = [
-  {
-    brand: 'Ford',
-    model: 'Fiesta',
-    color: 'red',
-    trims: ['titanium', 'st', 'active'],
-  },
-  {
-    brand: 'Peugeot',
-    model: '208',
-    color: 'blue',
-    trims: ['allure', 'GT'],
-  },
-  {
-    brand: 'Volkswagen',
-    model: 'Polo',
-    color: 'black',
-    trims: ['life', 'style', 'r-line'],
-  },
-]
+// const cars = [
+//   {
+//     brand: 'Ford',
+//     model: 'Fiesta',
+//     color: 'red',
+//     trims: ['titanium', 'st', 'active'],
+//   },
+//   {
+//     brand: 'Peugeot',
+//     model: '208',
+//     color: 'blue',
+//     trims: ['allure', 'GT'],
+//   },
+//   {
+//     brand: 'Volkswagen',
+//     model: 'Polo',
+//     color: 'black',
+//     trims: ['life', 'style', 'r-line'],
+//   },
+// ]
 
-for (let i = 0; i < cars.length; i++) {
-  if (cars[i].brand === 'Ford') {
-    cars[i].licensePlate = 'AA111BB'
-  } else if (cars[i].brand === 'Peugeot') {
-    cars[i].licensePlate = 'CC222DD'
-  } else {
-    cars[i].licensePlate = 'EE333FF'
-  }
-}
-console.log(cars)
+// for (let i = 0; i < cars.length; i++) {
+//   if (cars[i].brand === 'Ford') {
+//     cars[i].licensePlate = 'AA111BB'
+//   } else if (cars[i].brand === 'Peugeot') {
+//     cars[i].licensePlate = 'CC222DD'
+//   } else {
+//     cars[i].licensePlate = 'EE333FF'
+//   }
+// }
+// console.log(cars)
 
 /* ESERCIZIO 6 
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
-const obj = {
-  brand: 'Toyota',
-  model: 'CHR',
-  color: 'red',
-  trims: ['life', 'style', 'r-line'],
-  licensePlate: 'AA123BB',
-}
+// cars.push({
+//   brand: 'Toyota',
+//   model: 'CHR',
+//   color: 'red',
+//   trims: ['life', 'style', 'r-line'],
+//   licensePlate: 'AA123BB',
+// })
 
-cars.push(obj)
-
-for (let i = 0; i < cars.length; i++) {
-  cars[i].trims.pop()
-}
-console.log(cars)
+// for (let i = 0; i < cars.length; i++) {
+//   cars[i].trims.pop()
+// }
+// console.log(cars)
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
-const justTrims = []
+
+// const justTrims = []
+// for (let i = 0; i < cars.length; i++) {
+//   first = cars[i].trims[0]
+//   justTrims.push(first)
+// }
+
+// console.log(justTrims)
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 
-// for ( let i = 0; i < cars.length; i++){
-//   if()
-// }
-
-/* ESERCIZIO 9
-    Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
-*/
-const numericArray = [
-  6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
-]
-
-// let count = 0
-// while (count <= 32) {
-//   for (let i = 0; i > numericArray.length; i++) {
-//     console.log(count)
+// for (let i = 0; i < cars.length; i++) {
+//   if (cars[i].color.charAt(0) === 'b') {
+//     console.log('Fizz')
+//   } else {
+//     console.log('Buzz')
 //   }
 // }
 
-/* ESERCIZIO 10
+/* ESERCIZIO 9 FINIRE!!!
+    Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
+*/
+// const numericArray = [
+//   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
+// ]
+
+// let i = 0
+// while (numericArray[i] !== 32) {
+//   console.log(numericArray[i])
+//   i++
+// }
+
+/* ESERCIZIO 10 
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
-const charactersArray = ['g', 'n', 'u', 'z', 'd']
-const alphabet = []
+// const charactersArray = ['g', 'n', 'u', 'z', 'd']
+// const alphabet = []
 
-for (let i = 0; i < charactersArray.length; i++) {
-  switch (charactersArray[i]) {
-    case 'g':
-      alphabet.push(7)
-      break
-    case 'n':
-      alphabet.push(12)
-      break
-    case 'u':
-      alphabet.push(19)
-      break
-    case 'z':
-      alphabet.push(21)
-      break
-    case 'd':
-      alphabet.push(4)
-      break
-    default:
-  }
-}
+// for (let i = 0; i < charactersArray.length; i++) {
+//   switch (charactersArray[i]) {
+//     case 'g':
+//       alphabet.push(7)
+//       break
+//     case 'n':
+//       alphabet.push(12)
+//       break
+//     case 'u':
+//       alphabet.push(19)
+//       break
+//     case 'z':
+//       alphabet.push(21)
+//       break
+//     case 'd':
+//       alphabet.push(4)
+//       break
+//     default:
+//   }
+// }
 
-console.log(alphabet)
+// console.log(alphabet)
